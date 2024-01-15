9a8965507a5de4e2bfde59139b7a9b370f124326
GFT <gaaftechsolution@gmail.com>
2024-01-15 07:43:11 -0400

    initial commit

--- app.js
+++ app.js
@@ -1,22 +1,59 @@
+if (process.env.NODE_ENV !== 'production'){
+  require('dotenv').config()
+}
+
 const express = require('express');
-const router = require('./routes/user');
 const app = express();
-const path = require('path');
-const PORT = 3000;
+const PORT = 2000;
+const passport = require('passport');
+const initializePassport = require('./passport-config');
+const session = require('express-session');
+const flash = require('express-flash');
+const bcrypt = require('bcryptjs');
+const mongoose = require('mongoose');
 
-// Set view engine
+mongoose.connect(process.env.MONGODB_URL)
+.then(()=>{
+  console.log('connected to the database successfully')
+})
+.catch((err)=> {
+  console.error('error connecting to the database', err)
+})
+
+initializePassport(passport,
+  id => users.find(user => user.id === id),
+  email => users.find(user => user.email === email)
+)
+
+// midleware
+app.use(express.urlencoded({ extended: true }));
+app.use(flash())
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+}))
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.static('public'));
+
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views')); // Corrected the property to 'views'
 
-// // Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(router);
+const userRoute = require('./routes/user');
+const orderRoute = require('./routes/order');
+const adminRoute = require('./routes/admin');
 
-app.get('/', (req, res) => {
-  res.render('index');
+// ROUTE
+app.use('/user', userRoute);
+app.use('/order', orderRoute);
+app.use('/admin', adminRoute);
+
+
+app.get('/', (req, res)=>{
+  res.render('index')
 });
 
 
-app.listen(PORT, () => {
-  console.log(`App is running on: ${PORT}`);
-});
+app.listen(PORT,()=> {
+  console.log('server in now listening to: ',`${PORT}`)
+})
